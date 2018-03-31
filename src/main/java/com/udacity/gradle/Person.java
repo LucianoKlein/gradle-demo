06e6a6d7144f5d9cbb3a7c9419b482551b236899
LucianoKlein <kleinersilver@gmail.com>
2018-04-01 00:05:18 +0800

    gradle运行java

--- src/main/java/com/udacity/gradle/Person.java
+++ src/main/java/com/udacity/gradle/Person.java
@@ -3,7 +3,10 @@ package com.udacity.gradle;
 public class Person {
     private String name;
     private int age;
-    public void sayHello() {
+    public static void sayHello() {
         System.out.println("fuck you");
+    }
+    public static void main(String[] args) {
+        sayHello();
     }
 }
